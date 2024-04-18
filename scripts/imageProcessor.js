0efe60ffd5f853db9d281e2cd3ff1c058e08c665
YoshiYoshiPro <hiro.engineer0690@gmail.com>
2024-04-18 19:12:52 +0900

    fix: 🐛 fix

--- scripts/imageProcessor.js
+++ scripts/imageProcessor.js
@@ -4,21 +4,17 @@ const path = require("path");
 
 async function processImage(inputPath) {
   try {
-    const metadata = await sharp(inputPath).metadata();
     const outputPath = path.join(
       path.dirname(inputPath),
       "temp_" + path.basename(inputPath),
     );
 
-    let transformer = sharp(inputPath);
-
-    if (metadata.width < 1280 && metadata.height < 720) {
-      transformer = transformer.jpeg({ quality: 80 });
-    } else if (metadata.height > metadata.width) {
-      transformer = transformer.resize({ height: 720 }).jpeg({ quality: 80 });
-    } else {
-      transformer = transformer.resize({ width: 1280 }).jpeg({ quality: 80 });
-    }
+    let transformer = sharp(inputPath).resize({
+      width: 896,
+      height: 504,
+      fit: "inside",
+      withoutEnlargement: false,
+    });
 
     await transformer.toFile(outputPath);
 
