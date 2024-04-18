d9a6fa4c0005307b069b38f803a26edfcc73de3f
YoshiYoshiPro <hiro.engineer0690@gmail.com>
2024-04-18 18:52:14 +0900

    fix: 🐛 fix

--- scripts/imageProcessor.js
+++ scripts/imageProcessor.js
@@ -1,31 +1,33 @@
+const fs = require("fs");
 const sharp = require("sharp");
+const path = require("path");
 
 async function processImage(inputPath) {
   try {
     const metadata = await sharp(inputPath).metadata();
+    const outputPath = path.join(
+      path.dirname(inputPath),
+      "temp_" + path.basename(inputPath),
+    );
 
     let transformer = sharp(inputPath);
 
     if (metadata.width < 1280 && metadata.height < 720) {
-      // 画像のサイズが1280x720未満ならそのままJPEGで保存
       transformer = transformer.jpeg({ quality: 80 });
     } else if (metadata.height > metadata.width) {
-      // 縦が横より大きい場合、縦を720にリサイズ
       transformer = transformer.resize({ height: 720 }).jpeg({ quality: 80 });
     } else {
-      // 横が縦より大きい場合、横を1280にリサイズ
       transformer = transformer.resize({ width: 1280 }).jpeg({ quality: 80 });
     }
 
-    // 変更した画像を元のファイルに上書き保存
-    await transformer.toFile(inputPath);
+    await transformer.toFile(outputPath);
 
+    fs.renameSync(outputPath, inputPath);
     console.log("Image processed and saved:", inputPath);
   } catch (error) {
     console.error("Error processing image:", error);
   }
 }
 
-// コマンドラインからのファイルパスを取得
 const inputPath = process.argv[2];
 processImage(inputPath);
